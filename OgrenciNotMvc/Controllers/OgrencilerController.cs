38503c409b32df6cd7ca2a447148b66972337070
Dogan Alici <doganalici2144@gmail.com>
2024-01-30 22:03:58 +0300

    30.01.2024 Update

--- OgrenciNotMvc/Controllers/OgrencilerController.cs
+++ OgrenciNotMvc/Controllers/OgrencilerController.cs
@@ -9,27 +9,51 @@ namespace OgrenciNotMvc.Controllers
 {
     public class OgrencilerController : Controller
     {
-        DbMvcOkulEntities3 db=new DbMvcOkulEntities3();
+        DbMvcOkulEntities3 db = new DbMvcOkulEntities3();
         // GET: Ogrenciler
         public ActionResult Index()
         {
-            var Ogrenciler=db.TBLOGRENCILER.ToList();
+            var Ogrenciler = db.TBLOGRENCILER.ToList();
             return View(Ogrenciler);
         }
 
         [HttpGet]
         public ActionResult YeniOgrenci()
         {
+            List<SelectListItem> degerler=(from i in db.TBLKULUPLER.ToList()
+                                           select new SelectListItem
+                                           {
+                                               Text=i.KULUPAD,
+                                               Value=i.KULUPID.ToString()
+                                           }).ToList();
+            ViewBag.dgr = degerler;
             return View();
         }
         [HttpPost]
 
         public ActionResult YeniOgrenci(TBLOGRENCILER p3)
         {
+            var klp=db.TBLKULUPLER.Where(m=>m.KULUPID==p3.TBLKULUPLER.KULUPID).FirstOrDefault();
+            p3.TBLKULUPLER = klp;
             db.TBLOGRENCILER.Add(p3);
             db.SaveChanges();
-            return View();
+            return RedirectToAction("Index");
         }
     }
-
 }
+
+
+
+//List<SelectListItem> items = new List<SelectListItem>();
+
+//items.Add(new SelectListItem { Text = "Matematik", Value = "0" });
+
+//items.Add(new SelectListItem { Text = "Fen Bilgisi", Value = "1" });
+
+//items.Add(new SelectListItem { Text = "Atatürk İlke ve İnkılapları", Value = "2" });
+
+//items.Add(new SelectListItem { Text = "Coğrafya", Value = "3" });
+
+//items.Add(new SelectListItem { Text = "Edebiyat", Value = "4" });
+
+//ViewBag.DersAd = items;
